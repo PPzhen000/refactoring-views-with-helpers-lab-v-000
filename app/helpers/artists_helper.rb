c248389e45c2d98bd070f1b66cfdd05a7808e6c0
Ariel Zhen <zhenpingping@live.com>
2018-10-01 19:01:59 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,9 @@
 module ArtistsHelper
   def display_artist(song)
-    if !song.artist
-      link_to song
+    if song.artist
+      link_to artist.name, artist_path(song.artist)
     else
-      link_to song.artist
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
