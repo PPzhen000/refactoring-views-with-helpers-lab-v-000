3e210fe4e230d51aa517e5ba3262196973f421ee
Ariel Zhen <zhenpingping@live.com>
2018-10-01 18:53:36 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,9 @@
 module SongsHelper
   def display_artist
     if !song.artist_name
-
+      link_to 'Add Artist', song
     else
-
-    end 
+      link_to "Artist", song.artist 
+    end
   end
 end
